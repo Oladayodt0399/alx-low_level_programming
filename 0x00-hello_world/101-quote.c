a86cb6cefcc3340dcfadc55ce4846aef2a44fd41
Musa Baki <bakimusa8gmail.com>
2023-08-12 12:56:00 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - UNIX is basically a simple operating system
+ *
+ * Return: Always (1) (success)
+ */
+int main(void)
+{
+	putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
