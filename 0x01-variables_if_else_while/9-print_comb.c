9b6cf3f636193b5d042256288185905ba6b21b81
Musa Baki <bakimusa8gmail.com>
2023-08-13 04:24:49 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - printing all possible combinations of single-digit numbers
+ *
+ * Return: always (0)
+ *
+ */
+int main(void)
+{
+	for (int i = 0; i <= 9; i++)
+	{
+		putchar(i + '0');
+	}
+	if (i < 9)
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	return (0);
+}
