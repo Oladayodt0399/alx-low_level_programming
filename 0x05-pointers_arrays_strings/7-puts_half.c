2225e068e9a9fffce97bccc805dfe116070974aa
Musa Baki <bakimusa8gmail.com>
2023-08-21 14:30:07 -0700

    8-print_array.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * puts_half - a function that prints half of a string
+ * should print the second half of the string
+ * @n:
+ * Return: Always 0.
+ */
+
+void puts_half(char *str)
+{
+
