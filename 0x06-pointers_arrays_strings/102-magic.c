7b7fcbc2a07265b7313fde13729162b412950ccc
Musa Baki <bakimusa8gmail.com>
2023-08-24 15:39:58 -0700

    git push

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -5,7 +5,7 @@
  */
 int main(void)
 {
-	int n = 0;
+	int n;
 	int a[5];
 	int *p;
 
@@ -19,8 +19,7 @@ int main(void)
  * - only one statement
  * - you are not allowed to code anything else than this line of code
  */
-	n = 0;
-	*(p + 5) = 98;
+	*(p + 5 - sizeof(int) / sizeof(*p)) = 98;
 /* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
